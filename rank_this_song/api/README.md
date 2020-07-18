e78298370f21d6f76a90e36fc032ffeebea4c59b
ParmandeepChaddha <parmandeepchaddha@gmail.com>
2020-07-17 22:34:34 -0700

    -Adding more testing and starting to fix the discography component.

---
+++ rank_this_song/api/README.md
@@ -0,0 +1,28 @@
+## The Backend
+
+### Serving
+The backend for this application is built using Python and served using Flask.
+
+### Installing the Required Packages
+Currently, there's no package manager controlling the python modules being used in this app.That's in the works.
+Containerizing a new application takes time!
+
+### Getting to Hello World? 
+Have all the packages? Good.
+Now you can try to see if everything will work correctly by opening up command prompt (or anaconda prompt) in this terminal and running the following command: 
+` python -m pytest -v `
+If all your tests are successful, we are almost at hello world.
+
+Now run from the same terminal: 
+`python api.py`
+This will run and serve the Flask server (most probably on localhost:5000).
+
+Go to [link test] (localhost:5000) and you will have found the api. Yay!
+
+### TO DO: 
+For a list of TO DO activites for the backend please visit this link:
+[link text] (https://github.com/parmanchaddha/rank_this_song/projects)
+
+### Creator
+Parmandeep Chaddha
+Nathan Kredentser hasn't done shit yet.
--- rank_this_song/api/get_songs.py
+++ rank_this_song/api/get_songs.py
@@ -20,6 +20,8 @@ import spotipy
 from credentials import APP_CLIENT
 
 SONG_PAGE_PATTERN = "List_of_songs_recorded_by"
+DISCOGRAPHY_PATTERN = "discography"
+EXPECTED_SONG_COLUMNS = ["title", "song", "tracks", "name"]
 
 class CreateWikipediaConnection(object):
     """ 
@@ -29,52 +31,62 @@ class CreateWikipediaConnection(object):
         titled "List of Songs recorded by ______artist name____. 
     
     Args:
-        band_name (str): The name of the band being searched
+        artist_name (str): The name of the band being searched
         num_of_tracks (int): How many tracks to get for that band. No max enforced.
     
     Private Methods: 
-        __init__ (func): Initialize the wikipedia connection and find the wikipedia page.
-        _fetch_data_from_wikipedia (func): Fetch the HTML from wikipedia.
-        _parse_songs_from_html (func): Create a songs dataframe by parsing HTML table.
+        __init__ (func): 
+            Initialize the wikipedia connection
+            Find the wikipedia page
+            Get a list of tracks by an artist.
+
+        _get_song_page_exist (func): Check to see if "list_of_tracks_recorded_by_artist exists."
+        _fetch_song_data_from_wikipedia (func): Fetch the HTML from the artist song page.
         
+        _get_disc_page_exist (func): Check to see if "artist_discography" page exists.
+
     Public Methods: 
         get_songs (func): Get a list of songs from the wikipedia songs df. 
     
     Example Usage:
-        songs_wiki = CreateWikipediaConnection(band_name="The Beatles",
+        songs_wiki = CreateWikipediaConnection(artist_name="The Beatles",
                                                num_of_tracks=16)
         songs = songs_wiki.get_songs()
     """
-    def __init__(self, band_name:str = None, num_of_tracks:int=16):
+    def __init__(self, artist_name:str = None, num_of_tracks:int=16):
         """ Use Wikipedia to search for songs. """
-        self.band_name = band_name
+        self.artist_name = artist_name
         self.num_of_tracks = num_of_tracks
-        self.does_song_page_exist = self._get_song_page_exist(band_name)
-        if self.does_song_page_exist:
-            self.song_page_content = self._fetch_data_from_wikipedia(band_name)
-            self.songs_df = self._parse_songs_from_html(self.song_page_content)
-        else:
-            self.song_page_content = None
-            self.songs_df = pd.DataFrame(data=[], columns=["artist", "tracks"])
+        
+        if CreateWikipediaConnection._get_song_page_exist(artist_name):
+            self.songs_df = self._fetch_song_data_from_wikipedia(artist_name)
+            return
+
+         
+        print(CreateWikipediaConnection._get_disc_page_exist(artist_name))
+        
+        self.songs_df = pd.DataFrame(data=[], columns=["artist", "tracks"])
         return
 
-    def _get_song_page_exist(self, band_name:str=None): 
+
+    @staticmethod
+    def _get_song_page_exist(artist_name:str=None): 
         """
         Function: 
-            Check to see if `Songs Recorded by Band_Name` exists on Wikipedia
+            Check to see if `Songs Recorded by artist_name` exists on Wikipedia
             If it does, YAY! This makes it much easier to pull data.
         
         Return:
             does_song_page_exist (bool): Does the page exist? 
         """
-        if not band_name:
+        if not artist_name:
             raise Exception("Please enter a band name!")
 
         try:
-            check_page_url = wikipedia.page(f"List of Songs recorded by {band_name}").url
+            check_page_url = wikipedia.page(f"List of Songs recorded by {artist_name}").url
             if (SONG_PAGE_PATTERN in check_page_url 
-                and (band_name.lower() in check_page_url.lower()
-                     or band_name.replace(" ", "_").lower() in check_page_url.lower())):    
+                and (artist_name.lower() in check_page_url.lower()
+                     or artist_name.replace(" ", "_").lower() in check_page_url.lower())):    
                 does_song_page_exist = True
             else:
                 does_song_page_exist = False
@@ -83,28 +95,51 @@ class CreateWikipediaConnection(object):
         return does_song_page_exist
 
 
-    def _fetch_data_from_wikipedia(self, band_name:str = None):    
+    def _fetch_song_data_from_wikipedia(self, artist_name:str = None):    
         """ Fetch data from a wikipedia page based on a band name """
-        songs_page = wikipedia.page(f"List of Songs recorded by {band_name}")
+        songs_page = wikipedia.page(f"List of Songs recorded by {artist_name}")
         page_content = songs_page.html()
-        return page_content
-
-    def _parse_songs_from_html(self, html_string: str = None):
-        """ Parse song names from an HTML- Currently limited to table objects"""
-        list_of_tables_in_html = pd.read_html(html_string)
+        list_of_tables_in_html = pd.read_html(page_content)
         songs_df = None
         for table in list_of_tables_in_html:
-            if len([i for i in table.columns if str(i).lower() in ["title", "song", "name"]]):
+            if len([i for i in table.columns if str(i).lower() in EXPECTED_SONG_COLUMNS]):
                 print("Yay, found the songs table!")
                 songs_df = table
                 break
         return songs_df
+
+
+    @staticmethod
+    def _get_disc_page_exist(artist_name:str = None): 
+        """
+        Function:
+            Check to see if "artist_discography" page exists.
+
+        Args:
+            artist_name (str): Name of the artist whose songs are being searched.
+
+        Return:
+            does_disc_page_exist (bool): True if discography page exists. False otherwise.
+        """
+        if not artist_name:
+            raise Exception("Please enter a band name!")
+        
+        does_disc_page_exist = False
+        try:
+            check_page_url = wikipedia.page(f"{artist_name}_discography").url
+            if (DISCOGRAPHY_PATTERN in check_page_url
+                and (artist_name.lower() in check_page_url.lower()
+                     or artist_name.replace(" ", "_").lower() in check_page_url.lower())):    
+                does_disc_page_exist = True
+        except wikipedia.PageError:
+            pass
+        return does_disc_page_exist
     
     def get_songs(self):
         if (self.songs_df).empty:
             return []
         songs_col = [i for i in self.songs_df.columns if (
-                str(i).lower() in ["title", "song", "name"])][0]
+                str(i).lower() in EXPECTED_SONG_COLUMNS)][0]
         songs = self.songs_df[songs_col].astype(str).tolist()
         return songs
 
@@ -134,22 +169,22 @@ class CreateSpotfiyConnection(object):
         self.spotify_api = spotipy.Spotify(auth_manager=self.client)
         return
     
-    def get_tracks_by_band(self, band_name:str=None, num_of_tracks:int=16):
+    def get_tracks_by_band(self, artist_name:str=None, num_of_tracks:int=16):
         """ 
         Function:
             Get a list of songs by input band.
         
         Args: 
-            band_name (str): The name of the band being searched
+            artist_name (str): The name of the band being searched
             num_of_tracks (int): How many tracks to get for that band. Max = 50
         
         Return:
             songs (list): A list of songs by input band.
         """
-        if not band_name:
+        if not artist_name:
             raise Exception("Please add a band name in order to see a list of tracks")
-        search_query = r"artist:{}&type=track".format(band_name)
-        response = self.spotify_api.search(q=f"artist:{band_name}",
+        search_query = r"artist:{}&type=track".format(artist_name)
+        response = self.spotify_api.search(q=f"artist:{artist_name}",
                                            type="track",
                                            limit=num_of_tracks)
         songs = [i["name"] for i in response["tracks"]["items"]]
